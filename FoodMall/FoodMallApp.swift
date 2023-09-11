9e470efacff341dca37e386f25a27b277d2414c3
Harikrishnan K M <harikunjath407@gmail.com>
2023-09-11 18:10:30 +0530

    Initial Commit

---
+++ FoodMall/FoodMallApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMallApp.swift
+//  FoodMall
+//
+//  Created by Harikrishnan K M on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodMallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
